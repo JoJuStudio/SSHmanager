116cfd215713bd178accdba87b4795f90bf9879a
Johny <102170296+JoJuStudio@users.noreply.github.com>
2025-06-20 18:16:26 +0200

    Fix layout warnings and allow Ctrl+C to quit

--- sshmanager/main.py
+++ sshmanager/main.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import QApplication
 import sys
+import signal
 
 from .ui.main_window import MainWindow
 
 
 def main() -> None:
     app = QApplication(sys.argv)
+    signal.signal(signal.SIGINT, lambda *args: app.quit())
     win = MainWindow()
     win.show()
     sys.exit(app.exec())
